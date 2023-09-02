4730cdb909f8b437a0e00bffd953c97db46e24df
JennyGar <garbutje@sheridancollege.ca>
2023-09-02 17:06:07 -0400

    added case insensitivy and ratjam

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 load_dotenv()
 
 token=os.getenv("TEST_TOKEN")
-print(token)
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
@@ -33,10 +32,21 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('rat me'):
+    if message.content.startswith('RAT ME'):
+        newmessage = await message.channel.send('<:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851>')
+        newmessage = await message.channel.send('RATTING YOU UP SUPER STYLE')
+        newmessage = await message.channel.send('<:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851><:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851>')
+        await message.add_reaction('1256rat:1109634304310517851')
+        await message.add_reaction('a:ratjam:1147635795017076776')
+    elif message.content.lower() == 'rat me':
         newmessage = await message.channel.send('RATTING YOU UP')
         newmessage = await message.channel.send('<:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851> <:1256rat:1109634304310517851>')
         await message.add_reaction('1256rat:1109634304310517851')
         #await newmessage.add_reaction('1256rat:1109634304310517851')
+    elif message.content.lower() == 'jam me':
+        newmessage = await message.channel.send('JAMMING YOU UP')
+        newmessage = await message.channel.send('<a:ratjam:1147635795017076776><a:ratjam:1147635795017076776><a:ratjam:1147635795017076776><a:ratjam:1147635795017076776><a:ratjam:1147635795017076776>')
+        await message.add_reaction('<a:ratjam:1147635795017076776>')
+    
 
 client.run(token, log_handler=handler)
